c07ee50fba3b3891cb732c5c77cfeba61830b112
Fraser M. Callaghan <callaghan.fm@gmail.com>
2025-10-01 17:36:46 +0200

    disable tui 2 piwakawaka spline

--- tui/baseMarkupUI.py
+++ tui/baseMarkupUI.py
@@ -160,10 +160,12 @@ class BaseMarkupUI(object):
         self.imMarkupButton_A = QtWidgets.QPushButton(self.markupGroupBox)
         self.imMarkupButton_A.setObjectName("imMarkupButton_A")
         self.imageMarkupButtonsLayout.addWidget(self.imMarkupButton_A)
+        self.imMarkupButton_A.setEnabled(False) # STILL IN BETA
         
         self.imMarkupButton_B = QtWidgets.QPushButton(self.markupGroupBox)
         self.imMarkupButton_B.setObjectName("imMarkupButton_B")
         self.imageMarkupButtonsLayout.addWidget(self.imMarkupButton_B)
+        self.imMarkupButton_B.setEnabled(False) # STILL IN BETA
         
         # Markup mode controls
         self.markupModeLayout = QtWidgets.QVBoxLayout()
